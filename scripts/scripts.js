f788c788cf3a8399c50e36b5cd50bdd6b9cb98f7
Eric Rovtar <eric.rovtar@me.com>
2014-05-15 22:00:29 -0400

    specific data

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,7 +4,7 @@
 $(document).ready(function() {
     $(".share__tile").hover(
         function() {
-            $(".share__title--current").html("Hello!");
+            $(".share__title--current").html($(this).attr("data-service"));
         },
         function() {
             $(".share__title--current").html("");
